c944516645e67b7f26e689d96f792ebaac3a9ef2
jeannustre <jean.sarda@epitech.eu>
2017-05-04 02:29:23 +0200

    file upload

--- api/controllers/BookController.js
+++ api/controllers/BookController.js
@@ -7,6 +7,32 @@
 
 var Book = {
 
+  index: function(req, res) {
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end(
+      '<form action="http://localhost:1337/book/upload" enctype="multipart/form-data" method="post">'+
+      '<input type="text" name="title"><br>'+
+      '<input type="file" name="archive" multiple="multiple"><br>'+
+      '<input type="submit" value="Upload">'+
+      '</form>'
+    )
+  },
+
+  upload: function(req, res) {
+    req.file('archive').upload({
+      // 400MB
+      maxBytes: 400000000
+    }, function(err, files) {
+      if (err)
+        return res.serverError(err)
+      if (files.length === 0)
+        return res.badRequest('No file was uploaded')
+      return res.json({
+        message: files.length + ' file(s) uploaded successfully!',
+        files: files
+      })
+    })
+  }
 
 };
 
