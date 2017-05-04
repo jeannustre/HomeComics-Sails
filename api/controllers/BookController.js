8ad96ab5d1be47fba41e98e92634577bd6560ba3
jeannustre <jean.sarda@epitech.eu>
2017-05-04 04:13:47 +0200

    unzipping

--- api/controllers/BookController.js
+++ api/controllers/BookController.js
@@ -1,9 +1,14 @@
 /**
- * BookController
- *
- * @description :: Server-side logic for managing books
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
+* BookController
+*
+* @description :: Server-side logic for managing books
+* @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+*/
+
+var fs = require('fs')
+var stream = require('stream')
+var yauzl = require('yauzl')
+// see https://github.com/thejoshwolfe/yauzl/blob/master/examples/unzip.js
 
 var Book = {
 
@@ -20,20 +25,52 @@ var Book = {
 
   upload: function(req, res) {
     req.file('archive').upload({
-      // 400MB
-      maxBytes: 400000000
+      // 800MB
+      maxBytes: 800000000
     }, function(err, files) {
-      if (err)
+      if (err) {
         return res.serverError(err)
-      if (files.length === 0)
+      }
+      if (files.length === 0) {
         return res.badRequest('No file was uploaded')
+      }
+
+      var path = files[0].fd
+      console.log('unzipping file at ' + path)
+      yauzl.open(path, {lazyEntries: true}, function(err, zipfile) {
+        if (err) throw err;
+        zipfile.readEntry();
+        zipfile.on("entry", function(entry) {
+          console.log('entry: ' + JSON.stringify(entry))
+          if (/\/$/.test(entry.fileName)) {
+            console.log('A')
+            zipfile.readEntry()
+            // Directory file names end with '/'.
+            // Note that entires for directories themselves are optional.
+            // An entry's fileName implicitly requires its parent directories to exist.
+          } else {
+            // file entry
+            console.log('B')
+            zipfile.openReadStream(entry, function(err, readStream) {
+              if (err) throw err;
+              readStream.on("end", function() {
+                zipfile.readEntry();
+              });
+              //readStream.pipe(fsomewhere);
+            });
+          }
+        });
+      })
+
+
       return res.json({
         message: files.length + ' file(s) uploaded successfully!',
         files: files
       })
-    })
-  }
 
-};
+    })
+
+  }
+}
 
 module.exports = Book;
