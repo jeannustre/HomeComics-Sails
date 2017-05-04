71935a3e9e292b22be057a609f1feb2f8d49d489
jeannustre <jean.sarda@epitech.eu>
2017-05-04 16:28:41 +0200

    unzipping uploaded zip with yauzl

--- api/controllers/BookController.js
+++ api/controllers/BookController.js
@@ -8,7 +8,107 @@
 var fs = require('fs')
 var stream = require('stream')
 var yauzl = require('yauzl')
+var path = require('path')
+var util = require('util')
+var Transform = require('stream').Transform
 // see https://github.com/thejoshwolfe/yauzl/blob/master/examples/unzip.js
+
+function mkdirp(dir, cb) {
+  if (dir === ".") return cb()
+  fs.stat(dir, function(err) {
+    if (err == null) return cb() // folder already exisrs
+    var parent = path.dirname(dir)
+    mkdirp(parent, function() {
+      process.stdout.write(dir.replace(/\/$/, "") + "/\n")
+      fs.mkdir(dir, cb)
+    })
+  })
+}
+
+function handleZipFile(err, zipfile) {
+  if (err) throw err;
+
+  var handleCount = 0
+  function incrementHandleCount() {
+    handleCount++
+  }
+  function decrementHandleCount() {
+    handleCount--
+    if (handleCount === 0) {
+      console.log("all input and output handles closed")
+    }
+  }
+
+  incrementHandleCount()
+  zipfile.on("close", function() {
+    console.log("closed input file")
+    decrementHandleCount()
+  })
+
+  zipfile.readEntry()
+
+  zipfile.on("entry", function(entry) {
+    if (/\/$/.test(entry.fileName)) {
+      // directory file names end with '/'
+      mkdirp(entry.fileName, function() {
+        if (err) throw err;
+        zipfile.readEntry()
+      })
+    } else {
+      // ensure parent directory exists
+      mkdirp(path.dirname(entry.fileName), function() {
+        zipfile.openReadStream(entry, function(err, readStream) {
+          if (err) throw err
+          // report progress through large files
+          var byteCount = 0
+          var totalBytes = entry.uncompressedSize
+          var lastReportedString = byteCount + "/" + totalBytes + " 0%"
+          process.stdout.write(entry.fileName + "..." + lastReportedString)
+          function reportString(msg) {
+            var clearString = ""
+            for (var i = 0; i < lastReportedString.length; i++) {
+              clearString += "\b"
+              if (i >= msg.length) {
+                clearString += " \b"
+              }
+            }
+            process.stdout.write(clearString + msg)
+            lastReportedString = msg
+          }
+          // report progress at 60hz
+          var progressInterval = setInterval(function() {
+            reportString(byteCount + "/" + totalBytes + " " + ((byteCount / totalBytes * 100) | 0) + "%")
+          }, 1000/60)
+          var filter = new Transform()
+          filter._transform = function(chunk, encoding, cb) {
+            byteCount += chunk.length
+            cb(null, chunk)
+          }
+          filter._flush = function(cb) {
+            clearInterval(progressInterval)
+            reportString("")
+            // delete the "..."
+            process.stdout.write("\b \b\b \b\b \b\n")
+            cb()
+            zipfile.readEntry()
+          }
+
+          // pump file contents
+          var writeStream = fs.createWriteStream(entry.fileName)
+          incrementHandleCount()
+          writeStream.on("close", decrementHandleCount)
+          readStream.pipe(filter).pipe(writeStream)
+
+        })
+      })
+    }
+
+
+
+  })
+
+
+}
 
 var Book = {
 
@@ -35,9 +135,10 @@ var Book = {
         return res.badRequest('No file was uploaded')
       }
 
-      var path = files[0].fd
+      var zipPath = files[0].fd
       console.log('unzipping file at ' + path)
-      yauzl.open(path, {lazyEntries: true}, function(err, zipfile) {
+      yauzl.open(zipPath, {lazyEntries: true}, handleZipFile)
+    /*  yauzl.open(path, {lazyEntries: true}, function(err, zipfile) {
         if (err) throw err;
         zipfile.readEntry();
         zipfile.on("entry", function(entry) {
@@ -60,7 +161,7 @@ var Book = {
             });
           }
         });
-      })
+      })*/
 
 
       return res.json({
