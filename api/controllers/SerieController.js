326be6282b72a1ef642edf914cc18feec61ff10b
jeannustre <jean.sarda@epitech.eu>
2017-05-05 21:20:58 +0200

    generated empty Author and Serie apis

---
+++ api/controllers/SerieController.js
@@ -0,0 +1,11 @@
+/**
+ * SerieController
+ *
+ * @description :: Server-side logic for managing series
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
